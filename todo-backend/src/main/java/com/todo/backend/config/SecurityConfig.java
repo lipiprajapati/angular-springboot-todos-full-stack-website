2474368b77392e65e99c06f909790f4cd85c9bd7
Lipi Prajapati <lipiprajapati727@gmail.com>
2026-04-05 13:00:40 +0530

    Jdk & springboot upgrade
    * updated jdk and mvn version
    
    * Update todo-backend to Java 25
    
    * updated files according to jdk upgrade

---
+++ todo-backend/src/main/java/com/todo/backend/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.todo.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable)
+						.authorizeHttpRequests(auth -> auth.requestMatchers("/**").permitAll()
+								.anyRequest().authenticated())
+				.httpBasic(Customizer.withDefaults());
+		return http.build();
+	}
+}
--- todo-backend/src/main/java/com/todo/backend/todo/controller/TodoController.java
+++ todo-backend/src/main/java/com/todo/backend/todo/controller/TodoController.java
@@ -8,37 +8,31 @@ import com.todo.backend.todo.modal.Todo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
 
 @CrossOrigin("http://localhost:4200")
 @RestController
+@RequestMapping("/user")
 public class TodoController {
 
 	@Autowired 
 	private TodoServiceImpl todoServiceImpl;
 	
-	@GetMapping("/users/{username}/todos")
+	@GetMapping("/{username}/todos")
 	public List<Todo> getAllTodos(@PathVariable String username){
 		return todoServiceImpl.getAll();
 	}
 	
-	@PutMapping("/users/{username}/todos/{id}")
+	@PutMapping("/{username}/todos/{id}")
 	public ResponseEntity<Todo> updateTodo(@PathVariable String username,@PathVariable long id,  @RequestBody  Todo todo){
 		Todo updatedTodo = todoServiceImpl.save(todo);
 		return new ResponseEntity<Todo>(updatedTodo, HttpStatus.OK);
 	}
 	
-	@PostMapping("/users/{username}/todos")
+	@PostMapping("/{username}/todos")
 	public ResponseEntity<Void> insertTodo(@PathVariable String username, @RequestBody  Todo todo){
 		Todo creattedTodo = todoServiceImpl.save(todo);
 		//get current resource URL & append
@@ -46,7 +40,7 @@ public class TodoController {
 		return ResponseEntity.created(uri).build();
 	}
 	
-	@DeleteMapping("/users/{username}/todos/{id}")
+	@DeleteMapping("/{username}/todos/{id}")
 	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id){
 		Todo todo = todoServiceImpl.deleteById(id);
 		if(todo!=null) {
