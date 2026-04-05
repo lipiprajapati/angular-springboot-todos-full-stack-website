c3f68b8a852f989bf610b0ab30c8b1de1203b0c0
Lipi Prajapati <lipiprajapati727@gmail.com>
2026-04-05 12:16:53 +0530

    updated jdk and mvn version

--- todo-backend/src/main/java/com/todo/backend/todo/modal/Todo.java
+++ todo-backend/src/main/java/com/todo/backend/todo/modal/Todo.java
@@ -2,69 +2,14 @@ package com.todo.backend.todo.modal;
 
 import java.util.Date;
 
-import org.springframework.stereotype.Component;
-
-@Component
+@Data
 public class Todo {
 	private long id;
 	private String username;
-	private String discription;
+	private String description;
 	private Date targetDate;
 	private boolean isDone;
-	
-	public Todo(long id, String username, String discription, Date targetDate, boolean isDone) {
-		super();
-		this.id = id;
-		this.username = username;
-		this.discription = discription;
-		this.targetDate = targetDate;
-		this.isDone = isDone;
-	}
-	
-	public Todo() {
-		super();
-	}
 
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getDiscription() {
-		return discription;
-	}
-
-	public void setDiscription(String discription) {
-		this.discription = discription;
-	}
-
-	public Date getTargetDate() {
-		return targetDate;
-	}
-
-	public void setTargetDate(Date targetDate) {
-		this.targetDate = targetDate;
-	}
-
-	public boolean isDone() {
-		return isDone;
-	}
-
-	public void setDone(boolean isDone) {
-		this.isDone = isDone;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	
-	public void setId(long id) {
-		this.id = id;
-	}
 
 	@Override
 	public int hashCode() {
